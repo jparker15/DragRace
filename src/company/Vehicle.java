000d15032d90b55296b232a4fafc2693f532a001
Jeremy <jeremyeparker@yahoo.com>
2021-09-12 23:54:50 -0400

    v

--- src/company/Vehicle.java
+++ src/company/Vehicle.java
@@ -1,2 +1,32 @@
-package company;public class Vehicle {
+package company;
+
+public class Vehicle {
+
+        int year;
+        String make;
+        String model;
+        String color;
+        Engine engine;
+
+        public Vehicle(int year, String make, String model, String color, Engine engine){
+            this.year = year;
+            this.make = make;
+            this.model = model;
+            this.color = color;
+            this.engine = engine;
+        }
+
+        public void start(){
+            engine.start();
+        }
+        public void stop(){
+            engine.stop();
+        }
+
+        @Override
+        public String toString(){
+            return "Year: " + year + " | " + "Make: " + make + " | " + "Model: "+ model + " | " + "Color: " + color + " | " + "Engine: " + engine + " | ";
+        }
+
+
 }
